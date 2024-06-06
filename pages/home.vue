bcb616699a9a9e0f0388a4f97316b87b60af3d43
Maria Eduarda Ferreira Batista <ferreirabatistam34@gmail.com>
2024-06-06 18:14:09 -0300

    feat: create home page

---
+++ pages/home.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-btn style="background-color: teal; color: white;" @click="navigateTo('add')">Cadastrar Produto</v-btn>
+          <v-btn @click="navigateTo('view')">Visualizar Produto</v-btn>
+          <v-btn @click="navigateTo('edit')">Editar Produto</v-btn>
+          <v-btn @click="navigateTo('delete')">Deletar Produto</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      navigateTo(route) {
+        this.$router.push({ name: route });
+      }
+    }
+  }
+  </script>
+  
---
+++ pages/settings.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-container>
+      <v-switch v-model="darkTheme" label="Tema Escuro"></v-switch>
+    </v-container>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        darkTheme: false
+      };
+    },
+    watch: {
+      darkTheme(val) {
+        this.$vuetify.theme.dark = val;
+      }
+    }
+  }
+  </script>
+  
