309481838f4d70c5a70f32a9da57b9b9e6aa84cd
Maria Eduarda Ferreira Batista <ferreirabatistam34@gmail.com>
2024-06-06 18:12:19 -0300

    feat: creating addProducts page

---
+++ pages/addProduct.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-container>
+      <v-form>
+        <v-text-field label="Nome do Produto" v-model="product.name"></v-text-field>
+        <v-text-field label="Descrição" v-model="product.description"></v-text-field>
+        <v-text-field label="Preço" v-model="product.price"></v-text-field>
+        <v-btn @click="saveProduct">Salvar</v-btn>
+      </v-form>
+    </v-container>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        product: {
+          name: '',
+          description: '',
+          price: ''
+        }
+      };
+    },
+    methods: {
+      saveProduct() {
+        // Código para salvar o produto
+      }
+    }
+  }
+  </script>
+  
